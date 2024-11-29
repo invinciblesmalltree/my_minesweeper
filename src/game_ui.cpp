e7050738e10bea87691578115c7f62110a1fb8f6
invinciblesmalltree <2581283397@qq.com>
2024-11-29 09:24:12 +0800

    轻微修改代码

--- src/game_ui.cpp
+++ src/game_ui.cpp
@@ -15,15 +15,19 @@ GameUI::GameUI(int w, int h, int n, QWidget *parent) : QWidget(parent), weight(w
     setFixedSize(30 * w, 30 * h + 30);
 
     auto minesLabel = make_unique<QLabel>(u"剩余雷数: "_s);
-    minesLabel->setStyleSheet("font-size: 20px;");
     auto minesCountLabel = make_unique<QLabel>(QString::number(mines));
-    minesCountLabel->setStyleSheet("font-size: 20px;");
     auto timeLabel = make_unique<QLabel>(u"时间: "_s);
-    timeLabel->setStyleSheet("font-size: 20px;");
     auto timeCountLabel = make_unique<QLabel>(u"0"_s);
+    auto timer = make_unique<QTimer>();
+    auto titleLabel = make_unique<QHBoxLayout>();
+    auto layout = make_unique<QVBoxLayout>();
+    auto gridLayout = make_unique<QGridLayout>();
+
+    minesLabel->setStyleSheet("font-size: 20px;");
+    minesCountLabel->setStyleSheet("font-size: 20px;");
+    timeLabel->setStyleSheet("font-size: 20px;");
     timeCountLabel->setStyleSheet("font-size: 20px;");
 
-    auto timer = make_unique<QTimer>();
     timer->start(1000);
     connect(timer.release(), &QTimer::timeout, [this, timeCountLabel = timeCountLabel.get()] {
         static int time = 0;
@@ -31,29 +35,34 @@ GameUI::GameUI(int w, int h, int n, QWidget *parent) : QWidget(parent), weight(w
         timeCountLabel->setText(QString::number(time));
     });
 
-    auto titleLabel = make_unique<QHBoxLayout>();
+    for (int i = 0; i < w; ++i)
+        for (int j = 0; j < h; ++j) {
+            auto button = make_unique<QPushButton>();
+            button->setFixedSize(30, 30);
+            button->setStyleSheet(uR"(
+                QPushButton {
+                    background-color: #C0C0C0;
+                    border-bottom: 3px solid #808080;
+                    border-left: 3px solid white;
+                    border-radius: 0px;
+                    border-right: 3px solid #808080;
+                    border-top: 3px solid white;
+                }
+                QPushButton:checked {
+                    background-color: #BEBEBE;
+                    border: 2px solid #A6A6A6;
+                }
+            )"_s);
+            button->setCheckable(true);
+            gridLayout->addWidget(button.release(), j, i);
+        }
+
     titleLabel->addWidget(minesLabel.release());
     titleLabel->addWidget(minesCountLabel.release());
     titleLabel->addStretch();
     titleLabel->addWidget(timeLabel.release());
     titleLabel->addWidget(timeCountLabel.release());
-
-    auto layout = make_unique<QVBoxLayout>();
     layout->addLayout(titleLabel.release());
-
-    auto gridLayout = make_unique<QGridLayout>();
-    for (int i = 0; i < w; ++i)
-        for (int j = 0; j < h; ++j) {
-            auto button = make_unique<QPushButton>();
-            button->setFixedSize(30, 30);
-            button->setStyleSheet("font-size: 20px;");
-            button->setCheckable(true);
-            button->setAutoExclusive(false);
-            gridLayout->addWidget(button.release(), j, i);
-        }
-
-
     layout->addLayout(gridLayout.release());
-
     setLayout(layout.release());
 }
