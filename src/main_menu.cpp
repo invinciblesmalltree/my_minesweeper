f93f0c73275ec748b94eaea6b9d4461fcca7b541
invinciblesmalltree <2581283397@qq.com>
2024-11-29 08:52:09 +0800

    修复界面显示背景异常

--- src/main_menu.cpp
+++ src/main_menu.cpp
@@ -14,7 +14,7 @@ using namespace Qt;
 
 MainMenu::MainMenu(QWidget *parent) : QWidget(parent) {
     setWindowTitle(u"挖地雷"_s);
-    setFixedSize(600, 800);
+    setFixedSize(1440, 810);
 
     auto titleLabel = make_unique<QLabel>(u"挖地雷"_s);
     auto startGameButton = make_unique<QPushButton>(u"开始游戏"_s);
@@ -22,6 +22,8 @@ MainMenu::MainMenu(QWidget *parent) : QWidget(parent) {
     auto rankingTableButton = make_unique<QPushButton>(u"排  行  榜"_s);
     auto instructionsButton = make_unique<QPushButton>(u"玩法说明"_s);
     auto aboutButton = make_unique<QPushButton>(u"关       于"_s);
+    auto mainLayout = make_unique<QVBoxLayout>();
+    auto mainWidget = make_unique<QWidget>();
     auto layout = make_unique<QVBoxLayout>(this);
 
     titleLabel->setStyleSheet(uR"(
@@ -44,7 +46,21 @@ MainMenu::MainMenu(QWidget *parent) : QWidget(parent) {
             QPushButton:hover { background-color: #28B4B8; }
             QPushButton:pressed { background-color: #22989C; }
         )"_s);
-    setStyleSheet(u"background-image: url(:/images/扫雷背景.png);"_s);
+    mainWidget->setObjectName(u"mainWidget"_s);
+    mainWidget->setStyleSheet(uR"(
+        QWidget#mainWidget {
+            background-color: rgba(255, 255, 255, 80%);
+            margin: 0 400;
+            border-radius: 20px;
+        }
+    )"_s);
+    setObjectName(u"mainMenu"_s);
+    setStyleSheet(uR"(
+        QWidget#mainMenu {
+            background-image: url(:/images/扫雷背景.png);
+            background-position: center center;
+        }
+    )"_s);
 
     connect(startGameButton.get(), &QPushButton::clicked, [this] { DifficultyMenu(this).exec(); });
     connect(AIPlayerButton.get(), &QPushButton::clicked, [this] { DifficultyMenu(this).exec(); });
@@ -52,10 +68,12 @@ MainMenu::MainMenu(QWidget *parent) : QWidget(parent) {
     connect(instructionsButton.get(), &QPushButton::clicked, [this] { InstructionsUI(this).exec(); });
     connect(aboutButton.get(), &QPushButton::clicked, [this] { AboutUI(this).exec(); });
 
-    layout->setAlignment(QFlags(AlignCenter));
-    layout->addWidget(titleLabel.release());
+    mainLayout->setAlignment(QFlags(AlignCenter));
+    mainLayout->addWidget(titleLabel.release());
     for (const auto button: {startGameButton.release(), AIPlayerButton.release(), rankingTableButton.release(),
                              instructionsButton.release(), aboutButton.release()})
-        layout->addWidget(button);
+        mainLayout->addWidget(button);
+    mainWidget->setLayout(mainLayout.release());
+    layout->addWidget(mainWidget.release());
     setLayout(layout.release());
 }
